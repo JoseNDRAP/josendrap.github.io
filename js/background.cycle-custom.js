05319ec5c4787f75b360b30c28493c30f52f1f83
JoseNDRAP <67408117+JoseNDRAP@users.noreply.github.com>
2020-07-01 21:01:57 +0900

    Add files via upload

---
+++ js/background.cycle-custom.js
@@ -0,0 +1,31 @@
+// JavaScript Document
+
+var fadeShow = $(".background").fadeShow({
+
+
+
+	correctRatio: true,
+
+
+
+	shuffle: true,
+
+
+
+	speed: 4000,
+
+
+
+	images: ['images/bg-image.jpg',
+
+
+
+			 'images/bg-image-1.jpg']
+
+
+
+
+
+
+
+});
